bfc35061e08570d8159e3188724850b917307cd4
Grady King <grady.king07@gmail.com>
2021-03-15 17:12:06 -0400

    styling forecast page

--- client/src/components/CurrentReport/CurrentReport.jsx
+++ client/src/components/CurrentReport/CurrentReport.jsx
@@ -1,87 +1,118 @@
-import './CurrentReport.css'
-import { Link } from 'react-router-dom'
-import { Card, Button } from 'react-bootstrap'
-import { timeFormat } from '../../services/utils'
-
-
+import "./CurrentReport.css";
+import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
+import { timeFormat } from "../../services/utils";
+import copenhagenPhoto from "../../assets/copenhagen2.jpg";
 
 function CurrentReport(props) {
-  const { raleigh, newYork, copenhagen } = props
-  
-  
+  const { raleigh, newYork, copenhagen } = props;
+
   return (
-    <div className='current-section'>
-      <div className='title'>
+    <div className="current-section">
+      <div className="title">
         <h1>Current Weather</h1>
       </div>
 
-      <div className='report-container'>
-    <div className= 'report-card'>{raleigh && <Link to='/detail'>
-      <div>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/132984577_749384349270182_9004728231977785116_n.jpg?tp=1&_nc_ht=scontent.cdninstagram.com&_nc_cat=102&_nc_ohc=Sv-KhgZXjP0AX89AtVH&oh=11497e1933d75a7ed7de9a85c9ff03b4&oe=6069C538" />
-          <Card.Body>
-            <Card.Title>{raleigh.location.name}, {raleigh.location.region}</Card.Title>
-            <Card.Text>
-                  <h3>{Math.floor((raleigh.current.temperature * 9 / 5) + 32)}°
-              {raleigh.current.weather_descriptions[0]}</h3>
-              <h3>{raleigh.current.wind_speed} {raleigh.current.wind_dir}</h3>
-                  <h3>{timeFormat(raleigh.location.localtime)}</h3>
-              {/* <img src={raleigh.current.weather_icons[0]} /> */}
-            </Card.Text>
-            <Button variant="primary">Today's Forecast</Button>
-          </Card.Body>
-        </Card>
-      </div>
-    </Link>}
-      </div>
-      
-      <div className= 'report-card'>{newYork && <Link to='/detail'>
-      <div>
-        <Card  style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/121199423_817695555717838_1618265885191395506_n.jpg?tp=1&_nc_ht=scontent.cdninstagram.com&_nc_cat=110&_nc_ohc=495aYkGw28AAX-O76iQ&oh=2a518000e2c882b727b1e1df39983aca&oe=6068758D" />
-          <Card.Body>
-            <Card.Title>{newYork.location.name}, {newYork.location.region}</Card.Title>
-            <Card.Text>
-                  <h3>{Math.floor((newYork.current.temperature * 9 / 5) + 32)}°
-              {newYork.current.weather_descriptions[0]}</h3>
-              <h3>{newYork.current.wind_speed} {newYork.current.wind_dir}</h3>
-              <h3>{timeFormat(newYork.location.localtime)}</h3>
-              {/* <img src={newYork.current.weather_icons[0]} /> */}
-            </Card.Text>
-            <Button variant="primary">Today's Forecast</Button>
-          </Card.Body>
-        </Card>
-      </div>
-    </Link>}
-      </div>
+      <div className="report-container">
+        <div className="report-card">
+          {raleigh && (
+            <Link to="/detail/raleigh">
+              <div>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/132984577_749384349270182_9004728231977785116_n.jpg?tp=1&_nc_ht=scontent.cdninstagram.com&_nc_cat=102&_nc_ohc=Sv-KhgZXjP0AX89AtVH&oh=11497e1933d75a7ed7de9a85c9ff03b4&oe=6069C538"
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      {raleigh.location.name}, {raleigh.location.region}
+                    </Card.Title>
+                    <Card.Text>
+                      <h5>
+                        {Math.floor((raleigh.current.temperature * 9) / 5 + 32)}
+                        °{raleigh.current.weather_descriptions[0]}
+                      </h5>
+                      <h5>
+                        Wind: {raleigh.current.wind_speed}{" "}
+                        {raleigh.current.wind_dir}
+                      </h5>
+                      <h5>{timeFormat(raleigh.location.localtime)}</h5>
+                    </Card.Text>
+                    <Button variant="primary">Today's Forecast</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Link>
+          )}
+        </div>
 
-      <div className= 'report-card'>{copenhagen && <Link to='/detail'>
-      <div>
-        <Card  style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://scontent.cdninstagram.com/v/t51.2885-15/e15/p480x480/125559165_712080873021804_1080195584354446808_n.jpg?tp=1&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=N7OchD6MljUAX-cTtvd&oh=4c055656c4a55fc027c64c62304495e6&oe=6043230D" />
-          <Card.Body>
-            <Card.Title>{copenhagen.location.name}, {copenhagen.location.region}</Card.Title>
-            <Card.Text>
-                  <h3>{Math.floor((copenhagen.current.temperature * 9 / 5) + 32)}°
-              {copenhagen.current.weather_descriptions[0]}</h3>
-              <h3>{copenhagen.current.wind_speed} {copenhagen.current.wind_dir}</h3>
-                  <h3>{timeFormat(copenhagen.location.localtime)}</h3>
-              {/* <img src={copenhagen.current.weather_icons[0]} /> */}
-            </Card.Text>
-            <Button variant="primary">Today's Forecast</Button>
-          </Card.Body>
-        </Card>
-      </div>
-    </Link>}
-      </div>
+        <div className="report-card">
+          {newYork && (
+            <Link to="/detail/newyork">
+              <div>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/121199423_817695555717838_1618265885191395506_n.jpg?tp=1&_nc_ht=scontent.cdninstagram.com&_nc_cat=110&_nc_ohc=495aYkGw28AAX-O76iQ&oh=2a518000e2c882b727b1e1df39983aca&oe=6068758D"
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      {newYork.location.name}, {newYork.location.region}
+                    </Card.Title>
+                    <Card.Text>
+                      <h5>
+                        {Math.floor((newYork.current.temperature * 9) / 5 + 32)}
+                        °{newYork.current.weather_descriptions[0]}
+                      </h5>
+                      <h5>
+                        Wind: {newYork.current.wind_speed}{" "}
+                        {newYork.current.wind_dir}
+                      </h5>
+                      <h5>{timeFormat(newYork.location.localtime)}</h5>
+                    </Card.Text>
 
+                    <Button variant="primary">Today's Forecast</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Link>
+          )}
+        </div>
+
+        <div className="report-card">
+          {copenhagen && (
+            <Link to="/detail/copenhagen">
+              <div>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={copenhagenPhoto} />
+                  <Card.Body>
+                    <Card.Title>
+                      {copenhagen.location.name}, {copenhagen.location.region}
+                    </Card.Title>
+                    <Card.Text>
+                      <h5>
+                        {Math.floor(
+                          (copenhagen.current.temperature * 9) / 5 + 32
+                        )}
+                        °{copenhagen.current.weather_descriptions[0]}
+                      </h5>
+                      <h5>
+                        Wind: {copenhagen.current.wind_speed}{" "}
+                        {copenhagen.current.wind_dir}
+                      </h5>
+                      <h5>{timeFormat(copenhagen.location.localtime)}</h5>
+                      {/* <img src={copenhagen.current.weather_icons[0]} /> */}
+                    </Card.Text>
+                    <Button variant="primary">Today's Forecast</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Link>
+          )}
+        </div>
+      </div>
     </div>
-    </div>
-    
-
-  )
+  );
 }
 
-export default CurrentReport
-
+export default CurrentReport;
--- client/src/screens/Detail/Detail.jsx
+++ client/src/screens/Detail/Detail.jsx
@@ -1,3 +1,4 @@
+import "./Detail.css";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import axios from "axios";
@@ -6,6 +7,8 @@ import { useParams } from "react-router-dom";
 import { forecastURL } from "../../services/index";
 import { Card, CardDeck } from "react-bootstrap";
 import sun from "../../assets/sun2.jpg";
+import moon from "../../assets/Moon.jpg";
+import weather from "../../assets/Weather.jpg";
 import { timeFormat } from "../../services/utils";
 
 function Detail() {
@@ -32,60 +35,55 @@ function Detail() {
     <div>
       <NavBar />
       <div className="forecast-container">
-        <div>
+        <div className="forecast-title">
           <h1>
             {forecast.location.name}, {forecast.location.region}
           </h1>
         </div>
-        <CardDeck>
-          <Card>
-            <Card.Img variant="top" src={sun} />
-            <Card.Body>
-              <Card.Title>Current</Card.Title>
-              <Card.Text>
-                <h5>
-                  {Math.floor((forecast.current.temperature * 9) / 5 + 32)}°{" "}
-                  {forecast.current.weather_descriptions[0]}
-                </h5>
-                <h5>
-                  Wind: {forecast.current.wind_speed} mph{" "}
-                  {forecast.current.wind_dir}
-                </h5>
-                <h5>{timeFormat(forecast.location.localtime)}</h5>
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to
-                additional content.{" "}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardDeck>
+
+        <div className="forecast-cards">
+          <div className="current-forecast-card">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={forecast.current.is_day === "yes" ? sun : moon}
+              />
+              <Card.Body>
+                <Card.Title>Current</Card.Title>
+                <Card.Text>
+                  <h5>
+                    {Math.floor((forecast.current.temperature * 9) / 5 + 32)}°{" "}
+                    {forecast.current.weather_descriptions[0]}
+                  </h5>
+                  <h5>
+                    Wind: {forecast.current.wind_speed} mph{" "}
+                    {forecast.current.wind_dir}
+                  </h5>
+                  <h5>{timeFormat(forecast.location.localtime)}</h5>
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </div>
+          <div className="forecast-card">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={weather} />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This card has even longer
+                  content than the first to show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
