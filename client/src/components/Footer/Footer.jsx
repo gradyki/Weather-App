895e697317c7dbf768d260cb5319a595d0d75a0c
Grady King <grady.king07@gmail.com>
2021-03-30 16:14:47 -0400

    li and gh added

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,16 @@
-import './Footer.css'
-
+import "./Footer.css";
+import linkedIn from "../../assets/linkedIn.png";
+import github from "../../assets/github.png";
 
 function Footer() {
   return (
-    <div className='footer-container'>
+    <div className="footer-container">
       <footer>
-        Footer
-        </footer>
-  
+        <img src={linkedIn} />
+        <img src={github} />
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
